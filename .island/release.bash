420ea94b4237a5ab5286fd60c9038b556a979aa3
Edouard DUPIN <yui.heero@gmail.com>
2024-05-20 09:59:21 +0200

    [DEV] update development tag

--- .island/release.bash
+++ .island/release.bash
@@ -1,4 +1,14 @@
 #!/bin/bash
+version_file="version.txt"
 
-mvn versions:set -DnewVersion=$(cat version.txt)
-
+# update the Maven version number
+mvn versions:set -DnewVersion=$(sed 's/dev/SNAPSHOT/g' $version_file)
+if grep -q "DEV" "$version_file"; then
+    # update all versions release of dependency
+    mvn versions:use-latest-releases
+    # update our manage dependency as snapshoot
+    mvn versions:use-latest-versions -Dincludes=kangaroo-and-rabbit
+else
+    # update our manage dependency as release (must be done before)
+    mvn versions:use-latest-releases -Dincludes=kangaroo-and-rabbit
+fi
