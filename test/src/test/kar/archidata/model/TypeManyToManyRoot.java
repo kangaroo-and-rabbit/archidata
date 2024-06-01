c412daa1ca946cd8212b6b648270fe8e1edf1722
Edouard DUPIN <yui.heero@gmail.com>
2024-06-01 13:09:53 +0200

    [TEST] simplify test model

--- test/src/test/kar/archidata/model/TypeManyToManyRoot.java
+++ test/src/test/kar/archidata/model/TypeManyToManyRoot.java
@@ -2,18 +2,12 @@ package test.kar.archidata.model;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
+import org.kar.archidata.model.GenericData;
+
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
-public class TypeManyToManyRoot {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(nullable = false, unique = true)
-	public Long id = null;
+public class TypeManyToManyRoot extends GenericData {
 
 	public String otherData;
 
--- test/src/test/kar/archidata/model/TypeManyToManyRootExpand.java
+++ test/src/test/kar/archidata/model/TypeManyToManyRootExpand.java
@@ -2,20 +2,14 @@ package test.kar.archidata.model;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
+import org.kar.archidata.model.GenericData;
+
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Table(name = "TypeManyToManyRoot")
-public class TypeManyToManyRootExpand {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(nullable = false, unique = true)
-	public Long id = null;
+public class TypeManyToManyRootExpand extends GenericData {
 
 	public String otherData;
 
