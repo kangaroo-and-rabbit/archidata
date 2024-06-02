1c41ea42731055005eeac811b4bf31afd9a55b49
Edouard DUPIN <yui.heero@gmail.com>
2024-06-02 21:27:53 +0200

    [FEAT] add a simple tool to generate random string (usefull in test)

---
+++ src/org/kar/archidata/tools/StringTools.java
@@ -0,0 +1,14 @@
+package org.kar.archidata.tools;
+
+public class StringTools {
+	public static String RandGeneratedStr(final int length) {
+		final String base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz0123456789éàê_- '()";
+		final StringBuilder out = new StringBuilder(length);
+		for (int iii = 0; iii < length; iii++) {
+			final int chId = (int) (base.length() * Math.random());
+			out.append(base.charAt(chId));
+		}
+		return out.toString();
+	}
+
+}
