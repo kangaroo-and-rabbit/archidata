ec283ee70d245990e7923761aae83d145794fe0e
Edouard DUPIN <yui.heero@gmail.com>
2024-09-25 20:25:10 +0200

    [FEAT] start think about no-sql

---
+++ src/org/kar/archidata/annotation/RESTORE.java
@@ -0,0 +1,19 @@
+package org.kar.archidata.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.ws.rs.HttpMethod;
+
+/**
+* Indicates that the annotated method responds to HTTP RESTORE requests.
+*
+* @author Edouard DUPIN
+* @see HttpMethod
+*/
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("RESTORE")
+public @interface RESTORE {}
--- src/org/kar/archidata/model/GenericData.java
+++ src/org/kar/archidata/model/GenericData.java
@@ -1,5 +1,7 @@
 package org.kar.archidata.model;
 
+import org.bson.types.ObjectId;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 public class GenericData extends GenericTiming {
+	@dev.morphia.annotations.Id
+	private ObjectId _id;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(nullable = false, unique = true)
