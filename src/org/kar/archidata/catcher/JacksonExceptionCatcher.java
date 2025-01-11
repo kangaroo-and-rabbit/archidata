cef06889ee2c4c864226dfa4dd27bb32a71ade28
Edouard DUPIN <yui.heero@gmail.com>
2025-01-11 10:42:00 +0100

    [DOC] add comment

--- src/org/kar/archidata/catcher/JacksonExceptionCatcher.java
+++ src/org/kar/archidata/catcher/JacksonExceptionCatcher.java
@@ -12,6 +12,13 @@ import jakarta.ws.rs.ext.ExceptionMapper;
 public class JacksonExceptionCatcher implements ExceptionMapper<JacksonException> {
 	private static final Logger LOGGER = LoggerFactory.getLogger(JacksonExceptionCatcher.class);
 
+	/**
+	 * This method is called when a JacksonException is thrown.
+	 * It logs the exception, builds a response with the error details, and returns it.
+	 *
+	 * @param exception the JacksonException that was thrown
+	 * @return a Response object containing the error details
+	 */
 	@Override
 	public Response toResponse(final JacksonException exception) {
 		LOGGER.warn("Catch exception Input data parsing:");
@@ -22,6 +29,12 @@ public class JacksonExceptionCatcher implements ExceptionMapper<JacksonException
 				.build();
 	}
 
+	/**
+	 * Builds a RestErrorResponse object from the given exception.
+	 *
+	 * @param exception the Exception that was thrown
+	 * @return a RestErrorResponse object containing the error details
+	 */
 	private RestErrorResponse build(final Exception exception) {
 		return new RestErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, "Catch JSON Exception",
 				exception.getMessage());
