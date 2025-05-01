828fc5a8982b33f71a8ffbd5ecdd5d196bcae51c
Edouard DUPIN <edouard.dupin@proton.me>
2025-05-01 14:32:08 +0200

    [FIX] update interceptor

--- src/main/org/atriasoft/archidata/annotation/checker/GroupCreate.java
+++ src/main/org/atriasoft/archidata/annotation/checker/GroupCreate.java
@@ -1,3 +1,6 @@
 package org.atriasoft.archidata.annotation.checker;
 
+/**
+ * Generic element for the group validation (create) must be used with @ValidGroup or in the API validation
+ */
 public interface GroupCreate {}
--- src/main/org/atriasoft/archidata/annotation/checker/GroupUpdate.java
+++ src/main/org/atriasoft/archidata/annotation/checker/GroupUpdate.java
@@ -1,3 +1,6 @@
 package org.atriasoft.archidata.annotation.checker;
 
+/**
+ * Generic element for the group validation (update) must be used with @ValidGroup or in the API validation
+ */
 public interface GroupUpdate {}
