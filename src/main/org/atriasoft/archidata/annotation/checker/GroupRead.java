688f11e46ab7c1c852c0322f6d28b163f04da102
Edouard DUPIN <edouard.dupin@proton.me>
2025-05-02 12:51:57 +0200

    [FEAT] add GroupRead annotation

---
+++ src/main/org/atriasoft/archidata/annotation/checker/GroupRead.java
@@ -0,0 +1,6 @@
+package org.atriasoft.archidata.annotation.checker;
+
+/**
+ * Generic element for the group validation (read) must be used with @ValidGroup or in the API validation.
+ */
+public interface GroupRead {}
