f7bb5686bd9174c8d6bdf70dae5c07529ec9b29b
Edouard DUPIN <edouard.dupin@proton.me>
2025-05-14 23:40:36 +0200

    [FEAT] add a validator to verify if a field is unique.

---
+++ src/main/org/atriasoft/archidata/annotation/checker/UniqueInBaseId.java
@@ -0,0 +1,33 @@
+package org.atriasoft.archidata.annotation.checker;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Constraint(validatedBy = UniqueInBaseIdValidator.class)
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueInBaseId {
+
+	String message() default "Value already exists in the DB.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	/**
+	 * Name Of the field in the class
+	 * @return Field name.
+	 */
+	String nameOfField() default "name";
+
+	/**
+	 * Need to determine the Class to access to the DB
+	 * @return the class to access on the DB
+	 */
+	Class<?> target();
+}
---
+++ src/main/org/atriasoft/archidata/annotation/checker/UniqueInBaseIdValidator.java
@@ -0,0 +1,57 @@
+package org.atriasoft.archidata.annotation.checker;
+
+import java.util.UUID;
+
+import org.atriasoft.archidata.checker.ValidationContext;
+import org.atriasoft.archidata.dataAccess.DataAccess;
+import org.atriasoft.archidata.dataAccess.QueryCondition;
+import org.atriasoft.archidata.dataAccess.options.Condition;
+import org.bson.types.ObjectId;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueInBaseIdValidator implements ConstraintValidator<UniqueInBaseId, String> {
+	String nameOfField;
+	Class<?> target;
+
+	@Override
+	public void initialize(final UniqueInBaseId annotation) {
+		this.nameOfField = annotation.nameOfField();
+		this.target = annotation.target();
+	}
+
+	@Override
+	public boolean isValid(final String value, final ConstraintValidatorContext context) {
+		if (value == null) {
+			return true;
+		}
+		final ValidationContext ctx = ValidationContext.current();
+		if (ctx == null) {
+			throw new RuntimeException("Context should be defined before call this validation!");
+		}
+		Condition checkOurselfCondition = null;
+		if (ctx.contains("oid")) {
+			final ObjectId id = ctx.get("oid", ObjectId.class);
+			checkOurselfCondition = id == null ? null : new Condition(new QueryCondition("_id", "!=", id));
+		} else if (ctx.contains("id")) {
+			final Long id = ctx.get("id", Long.class);
+			checkOurselfCondition = id == null ? null : new Condition(new QueryCondition("id", "!=", id));
+		} else if (ctx.contains("uuid")) {
+			final UUID id = ctx.get("uuid", UUID.class);
+			checkOurselfCondition = id == null ? null : new Condition(new QueryCondition("uuid", "!=", id));
+		}
+		try {
+			final long count = DataAccess.countWhere(this.target,
+					new Condition(new QueryCondition(this.nameOfField, "=", value)), checkOurselfCondition);
+			if (count != 0) {
+				return false;
+			}
+		} catch (final Exception e) {
+			context.buildConstraintViolationWithTemplate("fail to access on the DB").addConstraintViolation();
+			return false;
+		}
+		return true;
+
+	}
+}
