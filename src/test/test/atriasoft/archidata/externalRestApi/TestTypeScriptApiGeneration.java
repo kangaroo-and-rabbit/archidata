250991afdeea38501b843f187501f3a9255f7581
Edouard DUPIN <edouard.dupin@proton.me>
2025-05-17 09:28:14 +0200

    [FIX] add test for the typescript api generation

--- src/test/test/atriasoft/archidata/externalRestApi/TestTypeScriptApiGeneration.java
+++ src/test/test/atriasoft/archidata/externalRestApi/TestTypeScriptApiGeneration.java
@@ -9,7 +9,9 @@ import org.atriasoft.archidata.annotation.checker.GroupPersistant;
 import org.atriasoft.archidata.annotation.checker.GroupRead;
 import org.atriasoft.archidata.annotation.checker.GroupUpdate;
 import org.atriasoft.archidata.annotation.checker.ValidGroup;
+import org.atriasoft.archidata.annotation.method.ARCHIVE;
 import org.atriasoft.archidata.annotation.method.CALL;
+import org.atriasoft.archidata.annotation.method.RESTORE;
 import org.atriasoft.archidata.externalRestApi.AnalyzeApi;
 import org.atriasoft.archidata.externalRestApi.TsGenerateApi;
 import org.atriasoft.archidata.model.OIDGenericDataSoftDelete;
@@ -119,6 +121,22 @@ public class TestTypeScriptApiGeneration {
 		}
 	}
 
+	@Path("resourcePath")
+	public class SampleResourceArchive {
+		@ARCHIVE
+		public void archive(@PathParam("oid") final ObjectId oid) {}
+
+	}
+
+	@Path("resourcePath")
+	public class SampleResourceRestore {
+		@RESTORE
+		@Path("executionCall")
+		public Boolean restore(@PathParam("oid") final ObjectId oid) {
+			return null;
+		}
+	}
+
 	@Test
 	public void testGenerateABasicRestApiGet() throws Exception {
 
@@ -173,7 +191,7 @@ public class TestTypeScriptApiGeneration {
 				 */
 				import { z as zod } from "zod";
 				import { ZodLong } from "./long";
-				import { OIDGenericDataSoftDelete } from "./oid-generic-data-soft-delete";
+				import { ZodOIDGenericDataSoftDelete } from "./oid-generic-data-soft-delete";
 
 				export const ZodTestObject = ZodOIDGenericDataSoftDelete.extend({
 					valueEmpty: ZodLong.optional(),
@@ -194,7 +212,7 @@ public class TestTypeScriptApiGeneration {
 						ZodTestObject.parse(data);
 						return true;
 					} catch (e: any) {
-						console.log(`Fail to parse data type='TestObject' error=${e}`);
+						console.log(`Fail to parse data type='ZodTestObject' error=${e}`);
 						return false;
 					}
 				}
@@ -254,8 +272,8 @@ public class TestTypeScriptApiGeneration {
 				import { z as zod } from "zod";
 				import { ZodLong } from "./long";
 				import {
-					OIDGenericDataSoftDelete,
-					OIDGenericDataSoftDeleteCreate,
+					ZodOIDGenericDataSoftDelete,
+					ZodOIDGenericDataSoftDeleteCreate,
 				} from "./oid-generic-data-soft-delete";
 
 				export const ZodTestObject = ZodOIDGenericDataSoftDelete.extend({
@@ -277,7 +295,7 @@ public class TestTypeScriptApiGeneration {
 						ZodTestObject.parse(data);
 						return true;
 					} catch (e: any) {
-						console.log(`Fail to parse data type='TestObject' error=${e}`);
+						console.log(`Fail to parse data type='ZodTestObject' error=${e}`);
 						return false;
 					}
 				}
@@ -301,10 +319,421 @@ public class TestTypeScriptApiGeneration {
 						ZodTestObjectCreate.parse(data);
 						return true;
 					} catch (e: any) {
-						console.log(`Fail to parse data type='TestObjectCreate' error=${e}`);
+						console.log(`Fail to parse data type='ZodTestObjectCreate' error=${e}`);
 						return false;
 					}
 				}
 				""", generation.get(Paths.get("model/test-object.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiPut() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourcePut.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(15, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestJson,
+				} from "../rest-tools";
+				import {
+					TestObject,
+					TestObjectUpdate,
+					isTestObject,
+				} from "../model";
+
+				export namespace SampleResourcePut {
+					export function update({
+							restConfig,
+							data,
+						}: {
+						restConfig: RESTConfig,
+						data: TestObjectUpdate,
+					}): Promise<TestObject> {
+						return RESTRequestJson({
+							restModel: {
+								endPoint: "resourcePath/",
+								requestType: HTTPRequestModel.PUT,
+							},
+							restConfig,
+							data,
+						}, isTestObject);
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-put.ts")));
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import { z as zod } from "zod";
+				import { ZodLong } from "./long";
+				import {
+					ZodOIDGenericDataSoftDelete,
+					ZodOIDGenericDataSoftDeleteUpdate,
+				} from "./oid-generic-data-soft-delete";
+
+				export const ZodTestObject = ZodOIDGenericDataSoftDelete.extend({
+					valueEmpty: ZodLong.optional(),
+					valueNullGroupUpdate: ZodLong.optional(),
+					valueNullGroupCreate: ZodLong.optional(),
+					valueNullGroupOther: ZodLong.optional(),
+					valueNotNullValid: ZodLong,
+					valueNotNullGroupRead: ZodLong,
+					valueNotNullGroupUpdate: ZodLong.optional(),
+					valueNotNullGroupCreate: ZodLong.optional(),
+					valueNotNullGroupOther: ZodLong.optional(),
+				});
+
+				export type TestObject = zod.infer<typeof ZodTestObject>;
+
+				export function isTestObject(data: any): data is TestObject {
+					try {
+						ZodTestObject.parse(data);
+						return true;
+					} catch (e: any) {
+						console.log(`Fail to parse data type='ZodTestObject' error=${e}`);
+						return false;
+					}
+				}
+
+				export const ZodTestObjectUpdate = ZodOIDGenericDataSoftDeleteUpdate.extend({
+					valueEmpty: ZodLong.optional(),
+					valueNullGroupRead: ZodLong.optional(),
+					valueNullGroupCreate: ZodLong.optional(),
+					valueNullGroupOther: ZodLong.optional(),
+					valueNotNullValid: ZodLong,
+					valueNotNullGroupRead: ZodLong.optional(),
+					valueNotNullGroupUpdate: ZodLong,
+					valueNotNullGroupCreate: ZodLong.optional(),
+					valueNotNullGroupOther: ZodLong.optional(),
+				});
+
+				export type TestObjectUpdate = zod.infer<typeof ZodTestObjectUpdate>;
+
+				export function isTestObjectUpdate(data: any): data is TestObjectUpdate {
+					try {
+						ZodTestObjectUpdate.parse(data);
+						return true;
+					} catch (e: any) {
+						console.log(`Fail to parse data type='ZodTestObjectUpdate' error=${e}`);
+						return false;
+					}
+				}
+				""", generation.get(Paths.get("model/test-object.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiPatch() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourcePatch.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(15, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestJson,
+				} from "../rest-tools";
+				import {
+					TestObject,
+					TestObjectUpdate,
+					isTestObject,
+				} from "../model";
+
+				export namespace SampleResourcePatch {
+					export function patch({
+							restConfig,
+							data,
+						}: {
+						restConfig: RESTConfig,
+						data: Partial<TestObjectUpdate>,
+					}): Promise<TestObject> {
+						return RESTRequestJson({
+							restModel: {
+								endPoint: "resourcePath/",
+								requestType: HTTPRequestModel.PATCH,
+							},
+							restConfig,
+							data,
+						}, isTestObject);
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-patch.ts")));
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import { z as zod } from "zod";
+				import { ZodLong } from "./long";
+				import {
+					ZodOIDGenericDataSoftDelete,
+					ZodOIDGenericDataSoftDeleteUpdate,
+				} from "./oid-generic-data-soft-delete";
+
+				export const ZodTestObject = ZodOIDGenericDataSoftDelete.extend({
+					valueEmpty: ZodLong.optional(),
+					valueNullGroupUpdate: ZodLong.optional(),
+					valueNullGroupCreate: ZodLong.optional(),
+					valueNullGroupOther: ZodLong.optional(),
+					valueNotNullValid: ZodLong,
+					valueNotNullGroupRead: ZodLong,
+					valueNotNullGroupUpdate: ZodLong.optional(),
+					valueNotNullGroupCreate: ZodLong.optional(),
+					valueNotNullGroupOther: ZodLong.optional(),
+				});
+
+				export type TestObject = zod.infer<typeof ZodTestObject>;
+
+				export function isTestObject(data: any): data is TestObject {
+					try {
+						ZodTestObject.parse(data);
+						return true;
+					} catch (e: any) {
+						console.log(`Fail to parse data type='ZodTestObject' error=${e}`);
+						return false;
+					}
+				}
+
+				export const ZodTestObjectUpdate = ZodOIDGenericDataSoftDeleteUpdate.extend({
+					valueEmpty: ZodLong.optional(),
+					valueNullGroupRead: ZodLong.optional(),
+					valueNullGroupCreate: ZodLong.optional(),
+					valueNullGroupOther: ZodLong.optional(),
+					valueNotNullValid: ZodLong,
+					valueNotNullGroupRead: ZodLong.optional(),
+					valueNotNullGroupUpdate: ZodLong,
+					valueNotNullGroupCreate: ZodLong.optional(),
+					valueNotNullGroupOther: ZodLong.optional(),
+				});
+
+				export type TestObjectUpdate = zod.infer<typeof ZodTestObjectUpdate>;
+
+				export function isTestObjectUpdate(data: any): data is TestObjectUpdate {
+					try {
+						ZodTestObjectUpdate.parse(data);
+						return true;
+					} catch (e: any) {
+						console.log(`Fail to parse data type='ZodTestObjectUpdate' error=${e}`);
+						return false;
+					}
+				}
+				""", generation.get(Paths.get("model/test-object.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiDelete() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourceDelete.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(9, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPMimeType,
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestVoid,
+				} from "../rest-tools";
+				import {
+					ObjectId,
+				} from "../model";
+
+				export namespace SampleResourceDelete {
+					export function remove({
+							restConfig,
+							params,
+						}: {
+						restConfig: RESTConfig,
+						params: {
+							oid: ObjectId,
+						},
+					}): Promise<void> {
+						return RESTRequestVoid({
+							restModel: {
+								endPoint: "resourcePath/",
+								requestType: HTTPRequestModel.DELETE,
+								contentType: HTTPMimeType.TEXT_PLAIN,
+							},
+							restConfig,
+							params,
+						});
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-delete.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiCall() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourceCall.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(10, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestJson,
+				} from "../rest-tools";
+				import {
+					TestEnumNV,
+				} from "../model";
+
+				export namespace SampleResourceCall {
+					export function execute({
+							restConfig,
+							queries,
+							data,
+						}: {
+						restConfig: RESTConfig,
+						queries: {
+							helloParam?: string,
+						},
+						data: TestEnum,
+					}): Promise<boolean> {
+						return RESTRequestJson({
+							restModel: {
+								endPoint: "resourcePath/executionCall",
+								requestType: HTTPRequestModel.CALL,
+							},
+							restConfig,
+							queries,
+							data,
+						});
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-call.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiArchive() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourceArchive.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(9, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestVoid,
+				} from "../rest-tools";
+				import {
+					ObjectId,
+				} from "../model";
+
+				export namespace SampleResourceArchive {
+					export function archive({
+							restConfig,
+							params,
+						}: {
+						restConfig: RESTConfig,
+						params: {
+							oid: ObjectId,
+						},
+					}): Promise<void> {
+						return RESTRequestVoid({
+							restModel: {
+								endPoint: "resourcePath/",
+								requestType: HTTPRequestModel.ARCHIVE,
+							},
+							restConfig,
+							params,
+						});
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-archive.ts")));
+	}
+
+	@Test
+	public void testGenerateABasicRestApiRestore() throws Exception {
+
+		final AnalyzeApi api = new AnalyzeApi();
+		api.addAllApi(List.of(SampleResourceRestore.class));
+
+		final Map<java.nio.file.Path, String> generation = TsGenerateApi.generateApi(api);
+
+		for (final java.nio.file.Path elem : generation.keySet()) {
+			LOGGER.info("path= {}", elem);
+		}
+		Assertions.assertEquals(9, generation.size());
+		Assertions.assertEquals("""
+				/**
+				 * Interface of the server (auto-generated code)
+				 */
+				import {
+					HTTPRequestModel,
+					RESTConfig,
+					RESTRequestJson,
+				} from "../rest-tools";
+				import {
+					ObjectId,
+				} from "../model";
+
+				export namespace SampleResourceRestore {
+					export function restore({
+							restConfig,
+							params,
+						}: {
+						restConfig: RESTConfig,
+						params: {
+							oid: ObjectId,
+						},
+					}): Promise<boolean> {
+						return RESTRequestJson({
+							restModel: {
+								endPoint: "resourcePath/executionCall",
+								requestType: HTTPRequestModel.RESTORE,
+							},
+							restConfig,
+							params,
+						});
+					};
+				}
+				""", generation.get(Paths.get("api/sample-resource-restore.ts")));
 	}
 }
