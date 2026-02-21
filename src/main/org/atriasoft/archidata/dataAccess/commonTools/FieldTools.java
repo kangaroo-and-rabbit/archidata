b34d64b2691e16d705e0f06dd2e8c561aeb9d526
Edouard DUPIN <edouard.dupin@proton.me>
2026-02-21 10:21:39 +0100

    [CLEAN] remove dead proxy models and optimize FieldTools
    Remove 8 dead code files (verified unused in archidata + all external projects):
    
    - addOn/model/TableListObjectGeneric, TableListObjectGenericUpdateAt:
      never imported anywhere
    - addOn/model/TableObjectGeneric, TableObjectGenericUpdateAt:
      only used by OneToManyTools (itself dead)
    - commonTools/OneToManyTools, ManyToOneTools: never called anywhere
    - checker/model/TableObjectGeneric, TableObjectGenericUpdateAt:
      never imported anywhere
    
    Optimize FieldTools.setFieldAtNull(): replace N+1 round-trip pattern
    (gets all docs + loop updateById) with single atomic updateMany via
    MongoLinkManager.setFieldToNullWhere().

--- src/main/org/atriasoft/archidata/dataAccess/commonTools/FieldTools.java
+++ src/main/org/atriasoft/archidata/dataAccess/commonTools/FieldTools.java
@@ -1,36 +1,32 @@
 package org.atriasoft.archidata.dataAccess.commonTools;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
-import org.atriasoft.archidata.annotation.AnnotationTools;
-import org.atriasoft.archidata.dataAccess.DataAccess;
+import org.atriasoft.archidata.checker.DataAccessConnectionContext;
+import org.atriasoft.archidata.dataAccess.DBAccessMongo;
+import org.atriasoft.archidata.dataAccess.model.DbClassModel;
+import org.atriasoft.archidata.dataAccess.model.DbPropertyDescriptor;
+import org.atriasoft.archidata.dataAccess.mongo.MongoLinkManager;
 import org.atriasoft.archidata.exception.DataAccessException;
 
 public class FieldTools {
 
 	/**
-	 * Set a specific Field at the null value.
+	 * Set a specific field to null on ALL documents of the given class.
+	 * Uses a single atomic {@code updateMany} with {@code $unset}.
+	 *
 	 * @param clazz Class to update in the DB.
-	 * @param fieldName Name of the field to reset.
+	 * @param fieldName Name of the Java property to reset.
 	 */
 	public static <T> void setFieldAtNull(final Class<T> clazz, final String fieldName) throws Exception {
-		final Field field = clazz.getField(fieldName);
-		if (field == null) {
+		final DbClassModel model = DbClassModel.of(clazz);
+		final DbPropertyDescriptor fieldDesc = model.findByPropertyName(fieldName);
+		if (fieldDesc == null) {
 			throw new DataAccessException(
-					"Fail to find the file Name:'" + fieldName + "' in class: " + clazz.getCanonicalName());
+					"Cannot find field '" + fieldName + "' in " + clazz.getCanonicalName());
 		}
-		final Field primaryKeyField = AnnotationTools.getPrimaryKeyField(clazz);
-		if (primaryKeyField == null) {
-			throw new DataAccessException(
-					"Fail to find the primary field not found in class: " + clazz.getCanonicalName());
-		}
-		field.setAccessible(true);
-		final List<T> data = DataAccess.gets(clazz);
-		for (final T elem : data) {
-			field.set(elem, null);
-			final Object primaryKey = primaryKeyField.get(elem);
-			DataAccess.updateById(elem, primaryKey);
+		final String fieldColumn = fieldDesc.getFieldName(null).inTable();
+		try (DataAccessConnectionContext ctx = new DataAccessConnectionContext()) {
+			final DBAccessMongo db = ctx.get();
+			MongoLinkManager.setFieldToNullWhere(db, clazz, null, null, fieldColumn);
 		}
 	}
 }
